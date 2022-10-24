5e7de81128dcb712cee8478ade7da8345045a912
luisbanez <luismbanez@gmail.com>
2022-10-24 21:35:02 +0800

    IOStream partially done. u can now save scores.

--- Vocab Havoc/src/IOstream/Istream.java
+++ Vocab Havoc/src/IOstream/Istream.java
@@ -1,9 +1,130 @@
 package IOstream;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Luis
+ * 
+ * LEADERBOARD FORMAT
+ * 
+ * (frenzy mode)
+ * [FRENZY] // notice: enclosed in square brackets
+ * NAME, SCORE, TIME
+ * ...
+ * 
+ * (time attack mode)
+ * [TIME ATTACK]
+ * NAME, TIME
+ * ...
+ * 
  */
 public class Istream {
-
+    protected static String fileToString(String path) throws FileNotFoundException, IOException {
+        FileReader fr; 
+        fr = new FileReader(path);
+        
+        String result = "";
+        for (int c = fr.read(); c != -1 ;c = fr.read()) {
+            result += (char)c;
+        }       
+        
+        fr.close();
+        return result;
+    }
+    
+    // looks for a word in brackets in the string and returns the position at the end of it.
+    // if word is not found, return 0.
+//    protected static int getWordInBracketsIndex(String string, String word) {
+//        int wordStartIndex = 0; // position of '[' in "[FRENZY]"
+//        int wordEndIndex = 0; // position of ']' in "[FRENZY]"
+//        String wordTest = "";
+//        word = "[" + word + "]";
+//        
+//        while (!wordTest.equals(word)) {
+//            // find '['
+//            for (int i = wordEndIndex ; i < string.length(); i++) {
+//                if (string.charAt(i) == '[') {
+//                    wordStartIndex = i;
+//                    break;
+//                }        
+//            }
+//            
+//            System.out.println("here");
+//            
+//            // find ']'
+//            for (int i = wordStartIndex; i < string.length(); i++) {
+//                if (string.charAt(i) == ']') {
+//                    wordEndIndex = i;
+//                    break;
+//                }
+//            }
+//            
+//            System.out.println("here2");
+//            // check if word between brackets is equal to word
+//            for (int i = wordStartIndex; i < wordEndIndex + 1; i++) {
+//                wordTest += string.charAt(i);
+//            }
+//            
+//            // if the entire string has been checked and still no match
+//            if (wordEndIndex > word.length()) {
+//                if (wordEndIndex > string.length)
+//            }
+//        }
+//        
+//        return wordEndIndex + 1;
+//    }
+    
+    public static int getWordInBracketsIndex(String string, String word) {
+        int openBracketIndex = 0;  // position of '['
+        int closeBracketIndex = 0;    // position of ']'
+        String wordInBrackets = "[" + word + "]";
+        
+        // will use this variable to check if word found inside brackets is what we are looking for
+        String wordTest = "";
+        
+        while(!wordTest.equals(wordInBrackets)) {
+            // look for '['
+            boolean openBracketFound = false;
+            for (int i = closeBracketIndex; i < string.length(); i++) {
+                if (string.charAt(i) == '[') {
+                    openBracketIndex = i;
+                    openBracketFound = true;
+                    break;
+                }
+            }
+            if (!openBracketFound) {
+                return 0;
+            }
+            
+            // this part will not be reached if the opening bracket is not found
+            // look for ']'
+            boolean closeBracketFound = false;
+            for (int i = openBracketIndex; i < string.length(); i++) {
+                if (string.charAt(i) == ']') {
+                    closeBracketIndex = i;
+                    closeBracketFound = true;
+                    break;
+                }
+            }
+            if (!closeBracketFound) {
+                return 0;
+            }
+            
+            // this part will not be reached if either bracket is not found
+            // check if word in brackets is the word we want
+            wordTest = string.substring(openBracketIndex, closeBracketIndex + 1);
+            
+            // if its not the word we are looking for
+            if (!wordTest.equals(wordInBrackets)) {
+                wordTest = ""; // reset shit
+            }
+        }
+        
+        return closeBracketIndex + 1;
+    }
 }
--- Vocab Havoc/src/IOstream/Ostream.java
+++ Vocab Havoc/src/IOstream/Ostream.java
@@ -1,9 +1,71 @@
 package IOstream;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Luis
+ * 
+ * LEADERBOARD FORMAT
+ * 
+ * [FRENZY] // notice: surrounded by brackets
+ * NAME, SCORE, TIME
+ * ...
+ * 
+ * [TIME ATTACK]
+ * NAME, TIME
+ * 
  */
 public class Ostream {
+    static String path = "savedata/leaderboard.txt";
+    
+    protected static void stringToFile(String data, String path) throws IOException {
+        File leaderboard = new File(path);
+        FileWriter fw = new FileWriter(leaderboard);
+        fw.write(data);
+        fw.close();
+    }
+    
+    public static void saveScoreFrenzy(String name, int wordsGuessed, String time) {
+        String scoreString = "\n" + name + "," + Integer.toString(wordsGuessed) + "," + time;
+        saveScore(scoreString, "FRENZY");
+    }
+    
+    public static void saveScoreTimeAttack(String name, String time) {
+        String scoreString = "\n" + name + "," + time;
+        saveScore(scoreString, "TIME ATTACK");
+    }
+    
+    protected static void saveScore(String score, String mode) {
+        // load the file as string so we can manipulate it
+        String leaderboard = "";
+        try {
+            leaderboard = Istream.fileToString(path);
+        } catch (IOException ex) {
+            System.out.println("failed to save score :(");
+            return;
+        }
 
+        int startIndex = Istream.getWordInBracketsIndex(leaderboard, mode);
+        
+        // we will append the new scores here
+        String startOfScores = leaderboard.substring(0, startIndex);
+        
+        // and then concat it with the rest of the string
+        String everythingElse = leaderboard.substring(startIndex);
+    
+        startOfScores += score;
+        leaderboard = startOfScores + everythingElse;
+        
+        // and then write the concatinated string to the save file
+        try {
+            stringToFile(leaderboard, path);
+        } catch (IOException ex) {
+            System.out.println("failed to save score :(");
+        }
+    }
 }
