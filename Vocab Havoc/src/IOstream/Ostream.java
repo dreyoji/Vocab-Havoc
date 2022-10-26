6152173c4cda1205f7506a510bf9069fe0050793
luisbanez <luismbanez@gmail.com>
2022-10-26 18:20:38 +0800

    internal `getRank` method for sorting the leaderboard

--- Vocab Havoc/src/IOstream/Ostream.java
+++ Vocab Havoc/src/IOstream/Ostream.java
@@ -3,6 +3,9 @@ package IOstream;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -40,15 +43,90 @@ public class Ostream {
     
     public static void saveScoreFrenzy(String name, int wordsGuessed, int time) {
         String scoreString = "\n" + name + "," + Integer.toString(wordsGuessed) + "/5," + numToTime(time);
-        saveScore(scoreString, "FRENZY");
+        float finalScore = getFinalScoreFrenzy(wordsGuessed, time);
+        saveScore(scoreString, "FRENZY", finalScore);
     }
     
     public static void saveScoreTimeAttack(String name, int time) {
         String scoreString = "\n" + name + "," + numToTime(time);
-        saveScore(scoreString, "TIME ATTACK");
+        //saveScore(scoreString, "TIME ATTACK");
     }
     
-    protected static void saveScore(String score, String mode) {
+    protected static float getFinalScoreFrenzy(int wordsGuessed, int time) {
+        float timeScore = 100.f/time;
+        float wordScore = wordsGuessed;
+        float finalScore = (timeScore + wordScore) * 10;        
+        
+        return finalScore;
+    }
+    
+    protected static float getFinalScoreFromString(String score, String mode) {
+        float finalScore = getFinalScoreFromString(score.split(","), mode);
+        
+        return finalScore;
+            
+    }
+    
+    protected static float getFinalScoreFromString(String[] scores, String mode) {
+        float finalScore = 0;
+        
+        switch (mode) {
+            case "FRENZY": {
+
+                int wordsGuessed = Character.getNumericValue(scores[1].charAt(0));
+
+                int min = Integer.parseInt(scores[2].substring(0, 2));
+                int sec = Integer.parseInt(scores[2].substring(3, 5));
+                int time = (min * 60) + sec;
+
+                finalScore = getFinalScoreFrenzy(wordsGuessed, time);
+            } break;
+            
+            case "TIME ATTACK": {
+                // smth
+            } break;
+        }
+        
+        return finalScore;
+            
+    }
+    
+    protected static int getRank(float finalScore, String mode) {
+        String[] score = Istream.getScore(1, mode);
+        if (score == null) return 1;
+        
+        ArrayList<String[]> scores = new ArrayList<String[]>();
+        for (int i = 2; score != null; i++) {
+            scores.add(score);
+            score = Istream.getScore(i, mode);
+        }
+        
+        Float[] finalScores = new Float[scores.size()];
+        for (int i = 0; i < finalScores.length; i++) {
+            finalScores[i] = getFinalScoreFromString(scores.get(i), mode);
+        }
+        
+        // sort finalScores
+        Arrays.sort(finalScores, Collections.reverseOrder());
+        
+        // find the rank
+        int prevIndex = 0;
+        int currentIndex = 0;
+        int result = 0;
+        for (int i = 0; i < finalScores.length; i++) {
+            prevIndex = currentIndex;
+            currentIndex = i;
+            if (finalScore > finalScores[i]) {
+                result = currentIndex;
+                break;
+            }
+            result = currentIndex + 1;
+        }
+        
+        return result + 1;
+    }
+    
+    protected static void saveScore(String scoreString, String mode, float finalScore) {
         // load the file as string so we can manipulate it
         String leaderboard = "";
         try {
@@ -62,11 +140,13 @@ public class Ostream {
         
         // we will append the new scores here
         String startOfScores = leaderboard.substring(0, startIndex);
+        // get nth newline from startIndex
+        int rank = getRank(finalScore, mode);
         
         // and then concat it with the rest of the string
         String everythingElse = leaderboard.substring(startIndex);
     
-        startOfScores += score;
+        startOfScores += scoreString;
         leaderboard = startOfScores + everythingElse;
         
         // and then write the concatinated string to the save file
@@ -76,4 +156,8 @@ public class Ostream {
             System.out.println("failed to save score :(");
         }
     }
+    
+    public static void main(String[] args) {
+        System.out.println(getRank(56.5f, "FRENZY"));
+     }
 }
