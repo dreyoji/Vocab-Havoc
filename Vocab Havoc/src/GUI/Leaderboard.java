b572ea89015353752c158e02ffbf70f7f26af2d8
Humbleble <jan.kristoffer2002@gmail.com>
2022-10-27 10:47:59 +0800

    Added Functionality
    Added functionality to the menu of the leaderboard form

--- Vocab Havoc/src/GUI/Leaderboard.java
+++ Vocab Havoc/src/GUI/Leaderboard.java
@@ -31,9 +31,18 @@ public class Leaderboard extends javax.swing.JFrame {
         Frenzy1 = new javax.swing.JLabel();
         Frenzy = new javax.swing.JLabel();
         Frenzy2 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
         Leaderboard = new javax.swing.JLabel();
         Background = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu2 = new javax.swing.JMenu();
+        jMenu5 = new javax.swing.JMenu();
+        jMenuItem8 = new javax.swing.JMenuItem();
+        jMenuItem9 = new javax.swing.JMenuItem();
+        jMenuItem10 = new javax.swing.JMenuItem();
+        jMenuItem11 = new javax.swing.JMenuItem();
+        jSeparator2 = new javax.swing.JPopupMenu.Separator();
+        jMenuItem12 = new javax.swing.JMenuItem();
         jMenu1 = new javax.swing.JMenu();
         jMenu3 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
@@ -43,10 +52,9 @@ public class Leaderboard extends javax.swing.JFrame {
         jMenuItem4 = new javax.swing.JMenuItem();
         jMenuItem5 = new javax.swing.JMenuItem();
         jMenuItem6 = new javax.swing.JMenuItem();
-        jSeparator1 = new javax.swing.JPopupMenu.Separator();
-        jMenuItem7 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Vocab Havoc");
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
@@ -139,11 +147,44 @@ public class Leaderboard extends javax.swing.JFrame {
         Frenzy2.setText("Zen");
         getContentPane().add(Frenzy2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 310, -1, -1));
 
+        jButton1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jButton1.setText("<");
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 30, -1));
+
         Leaderboard.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/LeaderTitle.png"))); // NOI18N
         getContentPane().add(Leaderboard, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         Background.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/Background.png"))); // NOI18N
         getContentPane().add(Background, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+
+        jMenu2.setText("Home");
+
+        jMenu5.setText("Play");
+
+        jMenuItem8.setText("Frenzy");
+        jMenu5.add(jMenuItem8);
+
+        jMenuItem9.setText("Zen");
+        jMenu5.add(jMenuItem9);
+
+        jMenuItem10.setText("Time Attack");
+        jMenu5.add(jMenuItem10);
+
+        jMenu2.add(jMenu5);
+
+        jMenuItem11.setText("Help");
+        jMenuItem11.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem11ActionPerformed(evt);
+            }
+        });
+        jMenu2.add(jMenuItem11);
+        jMenu2.add(jSeparator2);
+
+        jMenuItem12.setText("Exit");
+        jMenu2.add(jMenuItem12);
+
+        jMenuBar1.add(jMenu2);
 
         jMenu1.setText("Settings");
 
@@ -172,10 +213,6 @@ public class Leaderboard extends javax.swing.JFrame {
         jMenu4.add(jMenuItem6);
 
         jMenu1.add(jMenu4);
-        jMenu1.add(jSeparator1);
-
-        jMenuItem7.setText("Exit");
-        jMenu1.add(jMenuItem7);
 
         jMenuBar1.add(jMenu1);
 
@@ -183,6 +220,12 @@ public class Leaderboard extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenuItem11ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem11ActionPerformed
+        this.dispose();
+        Information1 Info = new Information1();
+        Info.setVisible(true);
+    }//GEN-LAST:event_jMenuItem11ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -225,21 +268,28 @@ public class Leaderboard extends javax.swing.JFrame {
     private javax.swing.JLabel Frenzy1;
     private javax.swing.JLabel Frenzy2;
     private javax.swing.JLabel Leaderboard;
+    private javax.swing.JButton jButton1;
     private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
     private javax.swing.JMenu jMenu3;
     private javax.swing.JMenu jMenu4;
+    private javax.swing.JMenu jMenu5;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem10;
+    private javax.swing.JMenuItem jMenuItem11;
+    private javax.swing.JMenuItem jMenuItem12;
     private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JMenuItem jMenuItem4;
     private javax.swing.JMenuItem jMenuItem5;
     private javax.swing.JMenuItem jMenuItem6;
-    private javax.swing.JMenuItem jMenuItem7;
+    private javax.swing.JMenuItem jMenuItem8;
+    private javax.swing.JMenuItem jMenuItem9;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane3;
-    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JSeparator jSeparator2;
     private javax.swing.JTable jTable1;
     private javax.swing.JTable jTable2;
     private javax.swing.JTable jTable3;
